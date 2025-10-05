c66551ef7fff1e9588fa8964c9ba2ff46bb230c1
Simoon Song <simoonsong1006@gmail.com>
2025-10-05 04:29:18 -0700

    ui-changes

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,6 +12,25 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            
+            // Set window size first
+            window.set_size(tauri::LogicalSize::new(400, 100))?;
+            window.set_decorations(false)?;
+
+            // Get screen size
+            let monitor = window.current_monitor()?.unwrap();
+            let screen_size = monitor.size();
+            
+            // Position at top right
+            let x = (screen_size.width as i32)/2-400; // 20px padding from right
+            let y = 30; // 20px from top
+            
+            window.set_position(tauri::PhysicalPosition::new(x, y))?;
+            
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
